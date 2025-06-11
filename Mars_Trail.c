7307b754eb2868db45b0fc2dce1c1a31d9be791a
john hoeksema <john@mercury.local>
2025-06-10 21:07:18 -0400

    woohoo!

--- Mars_Trail.c
+++ Mars_Trail.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+void greeting();
+void ending();
+
 int main() {
-	printf("Hello, World!\n");
+
+	int month = 1;
+
+	greeting();
+	while (month < 10) {
+		printf("It's month %d\n", month);
+		month++;
+	}
+	ending();
+	
 	return 0;
+}
+
+void greeting() {
+	printf("Get ur ass in that spaceship\n");
+}
+
+void ending() {
+	printf("That's all Folks!\n");
 }
