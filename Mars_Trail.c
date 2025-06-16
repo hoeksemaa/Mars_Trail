5fd0322603bc08df2a713bb65945e27aace4bcde
john hoeksema <john@mercury.local>
2025-06-15 21:01:17 -0400

    event and choice structs

--- Mars_Trail.c
+++ Mars_Trail.c
@@ -1,11 +1,34 @@
 #include <stdio.h>
 
+#define MAX_CHOICES 4
+
 void greeting();
 void ending();
+
+typedef struct {
+	int fuel;
+	int food;
+	int shields;
+} Ship;
+
+typedef struct {
+	char description[200];
+	int fuel_change;
+	int food_change;
+	int shields_change;
+} Choice;
+
+typedef struct {
+	char title[100];
+	char description[200];
+	Choice choices[MAX_CHOICES];
+	int num_choices;
+} Event;
 
 int main() {
 
 	int month = 1;
+	Spaceship Ship = {100, 100, 100};
 
 	greeting();
 	while (month < 10) {
