212d28c5c6abd855c776eb82c34c9487d4023bbb
john hoeksema <john@mercury.local>
2025-06-22 20:56:31 -0400

    super basic game loop done

--- Mars_Trail.c
+++ Mars_Trail.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #define MAX_CHOICES 4
+#define MAX_EVENTS 10
 
 typedef struct {
 	int fuel;
@@ -25,27 +27,35 @@ void ending();
 void print_ship_attributes(Ship *ship);
 void outcome_flee_pirates(Ship *ship);
 void outcome_fight_pirates(Ship *ship);
+Event* initialize_events();
+Event* get_random_event(Event* events, int num_events);
+int get_user_choice(Event* event);
 
 int main() {
 
 	int month = 1;
 	Ship spaceship = {100, 100, 100};
-	Event pirates = {
-		"PIRATE ATTACK",
-		"The pirates have spotted your ship; they look mean and hungry!",
-		2,
-		{
-			{"Fight", outcome_fight_pirates},
-			{"Flee", outcome_flee_pirates}
-		}	
-	};
+	Event* events = initialize_events();
+	int num_events = 1;
+	int user_choice;
+	Event* current_event;
+
+	srand(0);
 
 	greeting();
 	while (month < 10) {
 		printf("It's month %d\n", month);
-		// EVENT STUFF GOES HERE
-		// TOO TIRED TO WRITE IT RN
+		
+		current_event = get_random_event(events, num_events);
+		
+		printf("EVENT: %s\n", current_event->title);
+		printf("%s", current_event->description);
+
+		user_choice = get_user_choice(current_event);
+		current_event->choices[user_choice - 1].outcome(&spaceship);
+
 		print_ship_attributes(&spaceship);
+		
 		month++;
 	}
 	ending();
@@ -77,4 +87,39 @@ void outcome_fight_pirates(Ship *ship) {
 	ship->fuel -= 10;
 	ship->food -= 10;
 	ship->shields -= 70;
+}
+
+Event* initialize_events() {
+	static Event game_events[MAX_EVENTS];
+
+	game_events[0] = (Event){
+		"PIRATE ATTACK",
+		"The pirates have spotted your ship, and they look mean and hungry!",
+		2,
+		{
+			{"Fight", outcome_fight_pirates},
+			{"Flee", outcome_flee_pirates}
+		}
+	};
+
+	return game_events;
+}
+
+Event* get_random_event(Event* events, int num_events) {
+	int idx = rand() % num_events;
+	return &events[idx];
+}
+
+int get_user_choice(Event* event) {
+	int user_choice;
+
+	printf("What do you want to do?\n");
+	for (int i = 0; i < event->num_choices; i++) {
+		printf("%d. %s\n", 1 + i, event->choices[i].description);
+	}
+
+	printf("Enter your choice: ");
+	scanf("%d", &user_choice);
+
+	return user_choice;
 }
