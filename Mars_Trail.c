ae0e4f9e4f4dad7498ea091e4343113c004b2f34
john hoeksema <john@mercury.local>
2025-06-25 22:02:10 -0400

    2 new events

--- Mars_Trail.c
+++ Mars_Trail.c
@@ -4,12 +4,15 @@
 
 #define MAX_CHOICES 4
 #define MAX_EVENTS 10
+#define MAX_WIDTH 4
 
 typedef struct {
 	int fuel;
 	int food;
 	int shields;
 	int crystals;
+	int cigarettes;
+	int morale;
 } Ship;
 
 typedef struct {
@@ -28,10 +31,20 @@ void greeting();
 void good_ending();
 void bad_ending();
 void print_ship_attributes(Ship *ship);
+
+// all Event outcomes
 void outcome_flee_pirates(Ship *ship);
 void outcome_fight_pirates(Ship *ship);
 void outcome_rob_pizza(Ship *ship);
 void outcome_buy_pizza(Ship *ship);
+void outcome_leave(Ship *ship);
+void outcome_chew_him_out(Ship *ship);
+void outcome_smoke_together(Ship *ship);
+void outcome_dont_care(Ship *ship);
+void outcome_give_nothing(Ship *ship);
+void outcome_give_food(Ship *ship);
+void outcome_give_cigarette(Ship *ship);
+
 Event* initialize_events();
 Event* get_random_event(Event* events, int num_events);
 int get_user_choice(Event* event);
@@ -40,9 +53,9 @@ bool is_ship_alive(Ship *ship);
 int main() {
 
 	int month = 1;
-	Ship spaceship = {100, 100, 100, 100};
+	Ship spaceship = {100, 100, 100, 100, 0, 100};
 	Event* events = initialize_events();
-	int num_events = 2;
+	int num_events = 4;
 	int user_choice;
 	Event* current_event;
 
@@ -83,7 +96,7 @@ void greeting() {
 }
 
 void good_ending() {
-	printf("You safely touch down on the planet Mars! You become national heros back at home. Unfortunately, you're not back at home.\n");
+	printf("You safely touch down on the planet Mars! You become national heroes back at home. Unfortunately, you're not back at home.\n");
 }
 
 void bad_ending() {
@@ -92,10 +105,12 @@ void bad_ending() {
 
 void print_ship_attributes(Ship *ship) {
 	printf("Ship Status:\n");
-	printf("	Fuel:     %d\n", ship->fuel);
-	printf("	Food:     %d\n", ship->food);
-	printf("	Shields:  %d\n", ship->shields);
-	printf("        Crystals: %d\n", ship->crystals);
+	printf("	Fuel:       %*d\n", MAX_WIDTH, ship->fuel);
+	printf("	Food:       %*d\n", MAX_WIDTH, ship->food);
+	printf("	Shields:    %*d\n", MAX_WIDTH, ship->shields);
+	printf("        Crystals:   %*d\n", MAX_WIDTH, ship->crystals);
+	printf("        Cigarettes: %*d\n", MAX_WIDTH, ship->cigarettes);
+	printf("        Morale:     %*d\n", MAX_WIDTH, ship->morale);
 }
 
 void outcome_flee_pirates(Ship *ship) {
@@ -111,11 +126,44 @@ void outcome_fight_pirates(Ship *ship) {
 
 void outcome_rob_pizza(Ship *ship) {
 	ship->food += 50;
+	ship->fuel -= 10;
 }
 
 void outcome_buy_pizza(Ship *ship) {
 	ship->food += 50;
 	ship->crystals -= 50;
+	ship->fuel -= 10;
+}
+
+void outcome_leave(Ship *ship) {
+	ship->fuel -= 10;
+}
+
+void outcome_chew_him_out(Ship *ship) {
+	ship->morale -= 20;
+}
+
+void outcome_smoke_together(Ship *ship) {
+	ship->morale += 20;
+	ship->cigarettes += 1;
+}
+
+void outcome_dont_care(Ship *ship) {
+	ship->morale += 10;
+}
+
+void outcome_give_nothing(Ship *ship) {
+	ship->fuel -= 10;
+}
+
+void outcome_give_food(Ship *ship) {
+	ship->food -= 10;
+	ship->fuel += 30;
+}
+
+void outcome_give_cigarette(Ship *ship) {
+	ship->cigarettes -= 1;
+	ship->fuel += 300;
 }
 
 Event* initialize_events() {
@@ -133,11 +181,34 @@ Event* initialize_events() {
 
 	game_events[1] = (Event){
 		"PAPA ZORB'S PIZZA",
-		"You spot a rusty red shack nestled in an asteroid. \"Welcome to Papa Zorb's, home of the glorpiest pizza in the galaxy. What can i getcha?\"\n",
-		2,
+		"You spot a rusty red shack nestled in an asteroid.\"Welcome to Papa Zorb's, home of the glorpiest pizza in the galaxy. What can i getcha?\"\n",
+		3,
 		{
 			{"Rob the pizza", outcome_rob_pizza},
-			{"Buy the pizza", outcome_buy_pizza}
+			{"Buy the pizza", outcome_buy_pizza},
+			{"Leave emptyhanded", outcome_leave}
+		}
+	};
+
+	game_events[2] = (Event){
+		"CAUGHT REDHANDED",
+		"One night, you can't sleep. You decide to go on a walk around the ship instead. What's this?! You catch the doctor smoking! NASA treats everything flammable like the plague.",
+		3,
+		{
+			{"Chew him out", outcome_chew_him_out},
+			{"Fuck it. Pass me one", outcome_smoke_together},
+			{"Walk away. Pretend it didn't happen", outcome_dont_care}
+		}
+	};
+
+	game_events[3] = (Event){
+		"PLUG'S BUTT-UGLY FUEL TANKER",
+		"An old man with a white handlebar mustasche in an 18-wheeler fuel truck pulls up next to you. \"I've been moving this cargo for 3 weeks straight now. Got anything to take the edge off?\"",
+		3,
+		{
+			{"Sorry; supplies are tight", outcome_give_nothing},
+			{"Give him a tortilla", outcome_give_food},
+			{"Give him a cigarette", outcome_give_cigarette}
 		}
 	};
 
